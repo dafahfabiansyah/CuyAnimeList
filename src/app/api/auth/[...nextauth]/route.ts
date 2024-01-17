c0e1a473047b4452e87f21c3f6ee2197313a6eaa
dafahfabiansyah <davarobiansyah@gmail.com>
2024-01-18 02:32:54 +0700

    trying bug fixing upload to vercel

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,52 +1,52 @@
-// import NextAuth, { AuthOptions } from 'next-auth';
-// import GitHubProvider from 'next-auth/providers/github';
-// import GoogleAuth from 'next-auth/providers/google';
-
-// const authOptions: AuthOptions = {
-//   providers: [
-//     GitHubProvider({
-//       clientId: process.env.GITHUB_ID as string,
-//       clientSecret: process.env.GITHUB_SECRET as string,
-//     }),
-//     GoogleAuth({
-//       clientId: process.env.GOOGLE_CLIENT_ID as string,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-//     }),
-//   ],
-
-//   secret: process.env.NEXTAUTH_SECRET,
-// };
-
-// export default authOptions;
-
-// const handler = NextAuth(authOptions);
-
-// export { handler as GET, handler as POST };
-
-// auth-config.ts
 import NextAuth, { AuthOptions } from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 import GoogleAuth from 'next-auth/providers/google';
 
-const authOption = (): AuthOptions => {
-  const authOptions: AuthOptions = {
-    providers: [
-      GitHubProvider({
-        clientId: process.env.GITHUB_ID as string,
-        clientSecret: process.env.GITHUB_SECRET as string,
-      }),
-      GoogleAuth({
-        clientId: process.env.GOOGLE_CLIENT_ID as string,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      }),
-    ],
+const authOptions: AuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleAuth({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
 
-    secret: process.env.NEXTAUTH_SECRET,
-  };
-
-  return authOptions;
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
-const handler = NextAuth(authOption());
+// export default authOptions;
 
-export { handler as GET, handler as POST, authOption };
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
+
+// auth-config.ts
+// import NextAuth, { AuthOptions } from 'next-auth';
+// import GitHubProvider from 'next-auth/providers/github';
+// import GoogleAuth from 'next-auth/providers/google';
+
+// const authOption = (): AuthOptions => {
+//   const authOptions: AuthOptions = {
+//     providers: [
+//       GitHubProvider({
+//         clientId: process.env.GITHUB_ID as string,
+//         clientSecret: process.env.GITHUB_SECRET as string,
+//       }),
+//       GoogleAuth({
+//         clientId: process.env.GOOGLE_CLIENT_ID as string,
+//         clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+//       }),
+//     ],
+
+//     secret: process.env.NEXTAUTH_SECRET,
+//   };
+
+//   return authOptions;
+// };
+
+// const handler = NextAuth(authOption());
+
+// export { handler as GET, handler as POST, authOption };
--- src/service/auth-service.tsx
+++ src/service/auth-service.tsx
@@ -1,7 +1,8 @@
 import { getServerSession } from 'next-auth';
-import authOption from '@/app/api/auth/[...nextauth]/route';
+// import authOption from '@/app/api/auth/[...nextauth]/route';
 
 export const authUserSession = async () => {
-  const session = await getServerSession(authOption);
+  // const session = await getServerSession(authOption);
+  const session = await getServerSession();
   return session?.user;
 };
