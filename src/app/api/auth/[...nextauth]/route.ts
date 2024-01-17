75e22dc5cc4731121b5a6e8e1fd3799d9b99d8c8
dafahfabiansyah <davarobiansyah@gmail.com>
2024-01-18 02:25:51 +0700

    add validate user and try bug fixing upload to vercel

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,24 +1,52 @@
+// import NextAuth, { AuthOptions } from 'next-auth';
+// import GitHubProvider from 'next-auth/providers/github';
+// import GoogleAuth from 'next-auth/providers/google';
+
+// const authOptions: AuthOptions = {
+//   providers: [
+//     GitHubProvider({
+//       clientId: process.env.GITHUB_ID as string,
+//       clientSecret: process.env.GITHUB_SECRET as string,
+//     }),
+//     GoogleAuth({
+//       clientId: process.env.GOOGLE_CLIENT_ID as string,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+//     }),
+//   ],
+
+//   secret: process.env.NEXTAUTH_SECRET,
+// };
+
+// export default authOptions;
+
+// const handler = NextAuth(authOptions);
+
+// export { handler as GET, handler as POST };
+
+// auth-config.ts
 import NextAuth, { AuthOptions } from 'next-auth';
-import GitHubAuth from 'next-auth/providers/github';
+import GitHubProvider from 'next-auth/providers/github';
 import GoogleAuth from 'next-auth/providers/google';
 
-const authOptions: AuthOptions = {
-  providers: [
-    GitHubAuth({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string,
-    }),
-    GoogleAuth({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
+const authOption = (): AuthOptions => {
+  const authOptions: AuthOptions = {
+    providers: [
+      GitHubProvider({
+        clientId: process.env.GITHUB_ID as string,
+        clientSecret: process.env.GITHUB_SECRET as string,
+      }),
+      GoogleAuth({
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      }),
+    ],
 
-  secret: process.env.NEXTAUTH_SECRET,
+    secret: process.env.NEXTAUTH_SECRET,
+  };
+
+  return authOptions;
 };
 
-export default authOptions;
+const handler = NextAuth(authOption());
 
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
+export { handler as GET, handler as POST, authOption };
